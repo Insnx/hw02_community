762708e8f264428f7aeddaa052924143197d7fea
Valentin Bobylev <itspri@yandex.ru>
2022-03-04 01:45:05 -0600

    изменил админ.ру

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -15,5 +15,7 @@ class PostAdmin(admin.ModelAdmin):
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
