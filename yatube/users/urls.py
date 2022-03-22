473456e186b4727bfafe4b401afe5fb9356cda23
Valentin Bobylev <itspri@yandex.ru>
2022-03-22 13:37:03 -0500

    Обновляю

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordChangeDoneView
+from django.contrib.auth.views import (LoginView, LogoutView, 
+PasswordResetView, PasswordResetDoneView,
+PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 
 from . import views
@@ -21,12 +23,24 @@ urlpatterns = [
         'password_reset/',
         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
-        name='password_reset_form'
+        name='password_reset'
     ),
     path(
         'password_reset/done/',
-        PasswordChangeDoneView.as_view(
+        PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'),
         name='password_reset_done'
+    ),
+    path(
+        'auth/password_change/',
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
+        name='password_change'
+    ),
+    path(
+        'auth/password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
+        name='password_change_done'
     ),
 ]
