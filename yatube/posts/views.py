98ce528f19c57fd1588aaf5a57d044503e0b26aa
Valentin Bobylev <itspri@yandex.ru>
2022-03-24 11:41:13 -0500

    Upd

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -19,12 +19,12 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
-    paginator = Paginator(posts, 2)
+    paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': page_obj,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
 
